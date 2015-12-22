694a847d0ad899600d13e3daf93c86d3f145fcb1
clv-vmware <clv@vmware.com>
2015-12-22 16:19:46 +0800

    with level ,but there 's a bug

--- js/game.js
+++ js/game.js
@@ -1,3 +1,17 @@
+var level1 = [
+	//start, end , gap,  type, override
+	[0, 4000, 500, 'step'],
+	[6000, 13000, 800, 'ltr'],
+	[12000, 16000, 400, 'circle'],
+	[18200, 20000, 500, 'straight', {x : 150 } ],
+	[18200, 20000, 500, 'straight', { x : 100} ],
+	[18400, 20000, 500, 'straight', { x: 200 } ],
+	[22000, 25000, 400, 'wiggle', { x: 300} ],
+	[22000, 25000, 400, 'wiggle', { x: 200} ]
+
+
+];
+
 
 var OBJECT_PLAYER = 1,
 OBJECT_PLAYER_PROJECTILE = 2,
@@ -57,14 +71,55 @@ var sprites = {
 }; // end sprites
 
 var enemies = {
-	basic: {
-	x: 100,
+	straight: {
+	x: 0,
 	y : -50,
-	sprite : 'enemy_purple',
-	B: 100 ,
-	C: 2,
-	E: 100,
-	health: 20
+	sprite : 'enemy_ship',
+	health: 10,
+
+
+	E: 100
+
+},
+ltr: {
+	x: 0,
+	y: -100,
+	sprite: 'enemy_purple',
+	health: 10,
+	B: 200,
+	C: 1,
+	E: 200
+},
+circle: {
+	x: 400,
+	y: -50,
+	sprite: 'enemy_circle',
+	health: 10,
+	A:0,
+	B: -200,
+	C: 1,
+	E: 20,
+	F: 200,
+	G: 1,
+	H: Math.PI/2
+},
+wiggle: {
+	x: 100,
+	y: -50,
+	sprite: 'enemy_bee',
+	health: 20,
+	B: 100,
+	C: 4,
+	E: 100
+},
+step: {
+	x: 0,
+	y: -50,
+	sprite: 'enemy_ circle',
+	health: 10,
+	B: 300,
+	C: 1.5,
+	E: 60
 }
 };
 
@@ -167,6 +222,11 @@ var PlayerShip = function() {
 }
 PlayerShip.prototype = new Sprite();
 PlayerShip.prototype.type = OBJECT_PLAYER;
+PlayerShip.prototype.hit = function( damage) {
+	if(this.board.remove( this)){
+		loseGame();
+	}
+}
 
 var PlayerMissile = function(x, y) {
 	this.setup('missile', {vy: -700, damage: 10 } );
@@ -190,16 +250,25 @@ PlayerMissile.prototype.step = function(dt ){
 	}
 }
 
+//winGame
 
 
 var playGame = function() {
 	var board = new GameBoard();
 
-	board.add(new Enemy(enemies.basic));
-	board.add(new Enemy(enemies.basic, {x : 200}));
+
+
 	board.add(new PlayerShip());
+	board.add(new Level( level1, winGame ));
 	Game.setBoard(3, board);
 }
+var winGame = function() {
+	Game.setBoard(3, new TitleScreen("you win !", "press fire to play again ", playGame  ));
+}
+var loseGame = function() {
+	Game.setBoard(3, new TitleScreen("you lose! ", "press fire to play again ",playGame ));
+}
+
 
 var Enemy = function(blueprint, override) {
 	this.merge(this.baseParameters);

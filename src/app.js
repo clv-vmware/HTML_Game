ca1ee713f76a0d00c3abc4a25c3b7653e88f8bc6
lvyuanyuan <lvyuanyuan@lvyuanyuandeMacBook-Pro.local>
2016-10-22 15:39:43 +0800

    1022

--- src/app.js
+++ src/app.js
@@ -1,329 +1,231 @@
+function trace(){
+    cc.log(Array.prototype.join.call(arguments, ", "));
+}
 
-var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
-    ctor:function () {
-        //////////////////////////////
-        // 1. super init first
+var GameScene = cc.Scene.extend({
+    onEnter: function () {
         this._super();
-
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
-        var size = cc.winSize;
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2
-        });
-        this.addChild(this.sprite, 0);
-
-
-        return true;
+        var layer = new GameLayer();
+        this.addChild(layer);
     }
 });
 
-var BallLayer = cc.Layer.extend({
-    deltaX: 1,
-    ball: null,
-    frame: 0,
-    bg: null,
+var GameLayer = cc.Layer.extend({
+    mapPanel: null,
+    ui: null,
+
+    score: 0,
+    level: 0,
+    steps: 0,
+    limitStep: 0,
+    targetScore: 0,
     
     ctor: function () {
         this._super();
 
-        var size = cc.director.getWinSize();
-        var ball = new cc.Sprite(res.Profile_jpg);
-        ball.x = 0;
-        ball.y = size.height / 2;
-        this.addChild(ball, 1);
-        this.ball = ball;
-
-        this.bg = new cc.DrawNode();
-        this.addChild(this.bg);
-
-        // this.scheduleUpdate();
-        return true;
-    },
-
-    update: function () {
-        var size = cc.director.getWinSize();
-        this.ball.x += this.deltaX;
-
-        if (this.ball.x >= size.width || this.ball.x <= 0) {
-            this.deltaX *= -1;
-        }
-
-        this.ball.y = Math.sin(this.frame / 20) * 50 + size.height / 2;
-        this.bg.drawDot(new cc.Point(this.ball.x, this.ball.y), 2, cc.color(255, 0, 0));
-        this.frame++;
-
-    }
-});
-
-var SimpleActionLayer = cc.Layer.extend({
-    ctor: function () {
-        this._super();
-
-        var size = cc.director.getWinSize();
-        var ball = new cc.Sprite(res.Profile_jpg);
-        ball.x = 0;
-        ball.y = size.height / 2;
-        this.addChild(ball, 1);
-
-        var action = cc.moveBy(2, cc.p(size.width / 2, 0));
-        var reverse = cc.reverse(action);
-        var seq = cc.sequence(action, reverse);
-        ball.runAction(seq);
-        return true;
-    }
-});
-
-var TrickyActionLayer = cc.Layer.extend({
-    ctor: function () {
-        this._super();
-
-        var size = cc.director.getWinSize();
-        var ball = new cc.Sprite(res.Profile_jpg);
-        ball.x = 0;
-        ball.y = size.height / 2;
-        this.addChild(ball, 1);
-
-        var action = cc.moveBy(2, 0, -(size.height - ball.height / 2));
-        action.easing(cc.easeIn(2));
-        var back = action.clone().reverse();
-        back.easing(cc.easeBounceIn());
-        ball.runAction(cc.sequence(action, back));
-    }
-});
-
-var ControlActionLayer = cc.Layer.extend({
-    ctor: function () {
-        this._super();
-
-        var size = cc.director.getWinSize();
-        var ball = new cc.Sprite(res.Profile_jpg);
-        ball.x = 0;
-        ball.y = size.height / 2;
-        this.addChild(ball, 1);
-
-        var action = cc.moveBy(1, cc.p(size.width / 2, 0));
-        var callback = cc.callFunc(this.callback, this, "message");
-        var sequence = cc.sequence(action, callback);
-        ball.runAction(sequence);
-
-        // cc.audioEngine.playMusic(res.newsAudio, true);
-
-        cc.eventManager.addListener({
-            event: cc.EventListener.MOUSE,
-            onMounseDown: function (event) {
-                var pos = evnet.getLocation();
-                var target = event.getCurrentTarget();
-                if (event.getButton() === cc.EventMouse.BUTTON_RIGHT) {
-                    trace("onRightMouseDown at" + pos.x + " " + pos.y);
-                }
-                else if (event.getButton() === cc.EventMouse.BUTTON_LEFT) {
-                    trace("onLeftMouseDown at", pos.x + " " + pos.y);
-                }
-            },
-            onMouseMove: function (event) {
-                
-            },
-            onMouseUp: function (event) {
-
-            }
-
-        }, this);
+        var size = cc.winSize;
+        var bg = new cc.Sprite("res/bg.jpg");
+        this.addChild(bg, 1);
+        bg.x = size.width / 2;
+        bg.y = size.height / 2;
 
 
-        return true;
-    },
+        var clippingPanel = new cc.ClippingNode();
+        this.addChild(clippingPanel, 2);
+        this.mapPanel = new cc.Layer();
+        this.mapPanel.x = (size.width - Constant.CANDY_WIDTH*Constant.MAP_SIZE)/2;
+        this.mapPanel.y = (size.height - Constant.CANDY_WIDTH*Constant.MAP_SIZE)/2;
+        clippingPanel.addChild(this.mapPanel, 1);
 
-    callback: function(nodeExcutingAction, data) {
-        trace(nodeExcutingAction instanceof cc.Sprite, data);
-    }
-});
+        var stencil = new cc.DrawNode();
+        stencil.drawRect(cc.p(this.mapPanel.x,this.mapPanel.y), cc.p(this.mapPanel.x+Constant.CANDY_WIDTH*Constant.MAP_SIZE,this.mapPanel.y+Constant.CANDY_WIDTH*Constant.MAP_SIZE),
+            cc.color(0,0,0), 1, cc.color(0,0,0));
+        clippingPanel.stencil = stencil;
 
-var MouseEventLayer = cc.Layer.extend({
-    ctor: function () {
-        this._super();
-
-        if ('mouse' in cc.sys.capabilities) {
+        if ("touched" in cc.sys.capabilities) {
             cc.eventManager.addListener({
                 event: cc.EventListener.TOUCH_ONE_BY_ONE,
-                onTouchBegan: function (event) {
-                    var pos = event.getLocation();
-                    var target = event.getCurrentTarget();
-                    if (event.getButton() === cc.EventMouse.BUTTON_RIGHT) {
-                        // trace("onRightMouseDown at" + pos.x + " " + pos.y);
-                    }
-                    else if (event.getButton() === cc.EventMouse.BUTTON_LEFT) {
-                        // trace("onLeftMouseDown at", pos.x + " " + pos.y);
-                    }
-                },
-                onMouseMove: function (event) {
-                    var pos = event.getLocation(), target = event.getCurrentTarget();
-                    // trace("onMouseMove at: " + pos.x + " " + pos.y );
-                },
+                onTouchBegan: this._onTouchBegan.bind(this)
 
-                onMouseUp: function () {
-                    var pos = event.getLocation(), target = event.getCurrentTarget();
-                    // trace("onMouseUp at: " + pos.x + " " + pos.y );
-                }
-
-            }, this);
-
+            }, this.mapPanel);
         }
-    }
-});
-
-var AccelerometerLayer = cc.Layer.extend({
-    ctor: function () {
-        this._super();
-
-        var winSize = cc.director.getWinSize();
-
-        if ('accelerometer' in cc.sys.capabilities) {
-            cc.inputManager.setAccelerometerInterval(1 / 30);
-            cc.inputManager.setAccelerometerEnabled(true);
-
+        else {
             cc.eventManager.addListener({
-                event: cc.EventListener.ACCELERATION,
-                callback: function (accelerometerInfo, event) {
-                    var target = event.getCurrentTarget();
-                    cc.log('Accel x:' + accelerometerInfo.x + 'y: ' + accelerometerInfo.y + 'z: ' + accelerometerInfo.z + 'timestamp' + accelerometerInfo.timestamp);
+                event: cc.EventListener.MOUSE,
+                onMouseDown: this._onMouseDown.bind(this)
+            }, this.mapPanel);
+        }
 
-                    var w = winSize.width;
-                    var h = winSize.height;
+        this._init();
+        this.ui = new GameUI(this);
+        this.addChild(this.ui, 3);
 
-                    var x = w * accelerometerInfo.x + w / 2;
-                    var y = w * accelerometerInfo.y + h / 2;
+        return true;
+    },
+    
+    _onTouchBegan: function (touch, event) {
+        var column = Math.floor((touch.getLocation().x - this.mapPanel.x) / Constant.CANDY_WIDTH);
+        var row = Math.floor((touch.getLocation().y - this.mapPanel.y) / Constant.CANDY_WIDTH);
 
-                    x = x *0.2 + target._prevX * 0.8;
-                    y = y* 0.2 + target._prevY * 0.8;
+        this._popCandy(column, row);
+
+        return true;
+    },
+
+    _checkCandyExist: function(i, j){
+        if(i >= 0 && i < Constant.MAP_SIZE && j >= 0 && j < Constant.MAP_SIZE){
+            return true;
+        }
+        return false;
+    },
+    
+    _onMouseDown: function (event) {console.log('in mouse down');
+        var column = Math.floor((event.getLocationX() - this.mapPanel.x) / Constant.CANDY_WIDTH);
+        var row = Math.floor((event.getLocationY() - this.mapPanel.y) / Constant.CANDY_WIDTH);
+
+        this._popCandy(column, row);
+    },
+    
+    _popCandy: function (column, row) {
+        if (this.moving) return;
+
+        var joinCandys = [this.map[column][row]];
+        var index = 0;
+        var pushIntoCandys = function (element) {
+            if (joinCandys.indexOf(element) < 0) {
+                joinCandys.push(element);
+            }
+        };
+
+        while (index < joinCandys.length) {
+            var candy = joinCandys[index];
+
+            if(this._checkCandyExist(candy.column-1, candy.row) && this.map[candy.column-1][candy.row].type == candy.type){
+                pushIntoCandys(this.map[candy.column-1][candy.row]);
+            }
+            if(this._checkCandyExist(candy.column+1, candy.row) && this.map[candy.column+1][candy.row].type == candy.type){
+                pushIntoCandys(this.map[candy.column+1][candy.row]);
+            }
+            if(this._checkCandyExist(candy.column, candy.row-1) && this.map[candy.column][candy.row-1].type == candy.type){
+                pushIntoCandys(this.map[candy.column][candy.row-1]);
+            }
+            if(this._checkCandyExist(candy.column, candy.row+1) && this.map[candy.column][candy.row+1].type == candy.type){
+                pushIntoCandys(this.map[candy.column][candy.row+1]);
+            }
+
+            index++;
+        }
+
+        if (joinCandys.length <= 1) return;
+        console.log('in pop candy');
+        this.steps++;
+        this.moving = true;
+
+        for (var i = 0;i < joinCandys.length;i++) {
+            var candy = joinCandys[i];
+            this.mapPanel.removeChild(candy);
+            this.map[candy.column][candy.row] = null;
+        }
+
+        this.score += joinCandys.length * joinCandys.length;
+        this._generateNewCandy();
+        this._checkSuccessdOrFail();
+        
+    },
+
+    _checkSuccessdOrFail: function () {
+        if (this.score > this.targetScore) {
+            this.ui.showSuccess();
+            this.score += (this.limitStep - this.steps) * 30;
+
+            Storage.setCurrentLevel(this.level + 1);
+            Storage.setCurrentScore(this.score);
+
+            this.scheduleOnce(function () {
+                cc.director.runScene(new GameScene());
+            }, 3);
+        }
+        else if (this.steps >= this.limitStep) {
+            this.ui.showFail();
+            Storage.setCurrentLevel(0);
+            Storage.setCurrentScore(0);
+            this.scheduleOnce(function () {
+                cc.director.runScene(new GameScene())
+            }, 3);
+        }
+    },
+
+    _generateNewCandy: function () {console.log('in generate new candy');
+        var maxTime = 0;
+        for(var i = 0;i < Constant.MAP_SIZE; i++) {
+            var missCount = 0;
+            for (var j = 0;j < this.map[i].length; j++) {
+
+                var candy = this.map[i][j];
+                if (!candy) {
+                    var candy = Candy.createRandomType(i, Constant.MAP_SIZE + missCount);
+                    this.mapPanel.addChild(candy);
+                    candy.x = candy.column * Constant.CANDY_WIDTH + Constant.CANDY_WIDTH / 2;
+                    candy.y = candy.row * Constant.CANDY_WIDTH + Constant.CANDY_WIDTH / 2;
+
+                    this.map[i][candy.row] = candy;
+                    missCount++;
+
                 }
-            })
+                else {
+                    var fallLength = missCount;
+                    if (fallLength > 0) {
+                        var duration = Math.sqrt(2 * fallLength / Constant.FALL_ACCELERATION);
+
+                        if (duration > maxTime) {
+                            maxTime = duration;
+                        }
+
+                        var move = cc.moveTo(duration, candy.x, candy.y - Constant.CANDY_WIDTH * fallLength).easing(cc.easeIn(2));
+                        candy.runAction(move);
+                        candy.row -= fallLength;
+                        this.map[i][j] = null;
+                        this.map[i][candy.row] = candy;
+                    }
+                }
+            }
+
+            for (var j = this.map[i].length;j >= Constant.MAP_SIZE; j--) {
+                this.map[i].splice(j, 1);
+            }
         }
-    }
-});
 
-var ResumeLayer = cc.Layer.extend({
-    ctor: function () {
-        this._super();
-        this.scheduleUpdate();
-        this.schedule(this.scheduleTest, 1);
-        this.scheduleOnce(this.scheduleOnceTest, 3);
-
-        setTimeout(function () {
-            trace("pause", this.currentTime());
-            this.pause();
-        }.bind(this), 2000);
-
-        setTimeout(function () {
-            trace("resume", this.currentTime());
-            this.pause();
-        }.bind(this), 5000);
-
-        trace(this.currentTime());
-        this.frame = 0;
+        this.scheduleOnce(this._finishCandyFalls.bind(this), maxTime);
     },
-    update: function () {
-        this.frame++;
-        if (this.frame % 30 == 0) {
-            trace("update 30 times");
+
+    _finishCandyFalls: function () {
+        this.moving = false;
+    },
+    
+    _init: function () {
+        this.steps = 0;
+        this.level = Storage.getCurrentLevel();
+        this.score = Storage.getCurrentScore();
+        this.limitStep = Constant.levels[this.level].limitStep;
+        this.targetScore = Constant.levels[this.level].targetScore;
+
+        this.map = [];
+        for (var i = 0; i < Constant.MAP_SIZE; i++) {
+            var column = [];
+            for (var j = 0; j < Constant.MAP_SIZE; j++) {
+                var candy = Candy.createRandomType(i,j);
+                this.mapPanel.addChild(candy);
+                candy.x = i * Constant.CANDY_WIDTH + Constant.CANDY_WIDTH/2;
+                candy.y = j * Constant.CANDY_WIDTH + Constant.CANDY_WIDTH/2;
+                column.push(candy);
+            }
+            this.map.push(column);
         }
-    },
 
-    scheduleTest: function () {
-        trace("scheduleTest", this.currentTime());
-    },
-
-    scheduleOnceTest: function () {
-        trace("scheduleOnceTest", this.currentTime());
-    },
-
-    currentTime: function () {
-        return parseInt(new Date().getTime() / 1000);
-    }
-});
-
-var InaccuracyTestLayer = cc.Layer.extend({
-    
-    ctor: function () {
-        this._super();
-        var startTime = new Date().getTime();
-        var count = 0;
-        this.schedule(function () {
-            var timePass = new Date().getTime() - startTime;
-            count++;
-            var delta = timePass - (count * 100);
-            trace();
-        }, 0.1);
-    }
-});
-
-
-var MenuItemSpriteLayer = cc.Layer.extend({
-    ctor: function () {
-        this._super();
-
-        var spriteNormal = new cc.Sprite(res.Profile_jpg);
-        var spriteSelected = new cc.Sprite(res.Profile_jpg);
-        var spriteDisabled = new cc.Sprite(res.Profile_jpg);
-        var menuSprite = new cc.MenuItemSprite(spriteNormal, spriteSelected, spriteDisabled, this.startGame, this);
-
-        var menu = new cc.Menu(menuSprite);
-        this.addChild(menu);
-        menuSprite.setEnabled(false); // disable the button
-        // menuSprite.setEnabled(true)
-    },
-    
-    startGame: function () {
-        trace("this is MenuItemSpriteLayer ?", this instanceof MenuItemSpriteLayer);
-    }
-});
-
-var trace = function () {
-    cc.log(Array.prototype.join.call(arguments, ", "));
-};
-
-var MenuItemFontLayer = cc.Layer.extend({
-    ctor: function () {
-        this._super();
-
-        var menuFont = new cc.MenuItemFont("START GAME", this.startGame, this);
-        menuFont.fontSize = 32;
-        menuFont.fontName = "Arial";
-        var menu = new cc.Menu(menuFont);
-        this.addChild(menu);
-    },
-    
-    startGame: function () {
-        trace("start game button clicked!");
-    }
-});
-
-var HelloWorldScene = cc.Scene.extend({
-    onEnter:function () {
-        this._super();
-        var layer = new MenuItemFontLayer();
-        this.addChild(layer);
 
 
     }
+
+
+
 });
-
-

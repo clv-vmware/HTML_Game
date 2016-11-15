e7f2b34c77a6aa0de928a6db7b87afe1fb373308
clv <clv@vmware.com>
2016-11-15 22:36:45 +0800

    add index

--- snake/init.js
+++ snake/init.js
@@ -1,3 +1,44 @@
+// index page: select level
+var slug = document.querySelector("#slug");
+var worm = document.querySelector("#worm");
+var python = document.querySelector("#python");
+
+EventUtil.addHandler(slug, 'click', selectLevel);
+EventUtil.addHandler(worm, 'click', selectLevel);
+EventUtil.addHandler(python, 'click', selectLevel);
+
+
+// decide snake' speed
+var level;
+
+
+function selectLevel (e) {
+    console.log(e.target.id);
+    level = e.target.id;
+    runGameScene();
+}
+
+function runGameScene () {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // DRAW bg
 // var bg = new Background();
 // bg.drawGrid(GRID_SIZE);
